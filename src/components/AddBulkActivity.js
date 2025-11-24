9f4e81264e5fd0bda476fc7261ac773c50cc0c1d
gemkidsacademy <info@gemkidsacademy.com.au>
2025-11-24 19:45:43 +0500

    Update AddBulkActivity.js

--- src/components/AddBulkActivity.js
+++ src/components/AddBulkActivity.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "./AddBulkActivity.css";
 
 export default function AddBulkActivity() {
   const [file, setFile] = useState(null);
